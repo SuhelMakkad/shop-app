d521d02a153b21ec4e4630bc1fefad3acdd640c2
Suhel Makkad <MAKADSUHEL11@GMAIL.COM>
2022-05-16 22:45:13 +0530

    initalized home page

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
