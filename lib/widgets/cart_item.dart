f2b2a7f948e1a1f2c7cbdc6fc4a776833ad94789
SuhelMakkad <MAKADSUHEL11@GMAIL.COM>
2022-05-25 23:26:38 +0530

    showing conformation box when item deleted on cart screen

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -21,18 +21,49 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) {
+            return AlertDialog(
+              title: const Text("Are you sure?"),
+              content:
+                  const Text("Do you want to remove the item from the card"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: themeData.colorScheme.error,
+                  ).copyWith(),
+                  child: const Text("No"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(true);
+                  },
+                  child: const Text("Yes"),
+                ),
+              ],
+            );
+          },
+        );
+      },
       background: Container(
         margin: const EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 5,
         ),
-        color: Theme.of(context).colorScheme.error,
+        color: themeData.colorScheme.error,
         alignment: Alignment.centerRight,
         padding: const EdgeInsets.only(right: 20),
         child: const Icon(
@@ -50,15 +81,14 @@ class CartItem extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: ListTile(
             leading: CircleAvatar(
-              backgroundColor: Theme.of(context).colorScheme.primary,
+              backgroundColor: themeData.colorScheme.primary,
               child: Padding(
                 padding: const EdgeInsets.all(5),
                 child: FittedBox(
                   child: Text(
                     '\$$price',
                     style: TextStyle(
-                      color:
-                          Theme.of(context).primaryTextTheme.titleMedium!.color,
+                      color: themeData.primaryTextTheme.titleMedium!.color,
                     ),
                   ),
                 ),
