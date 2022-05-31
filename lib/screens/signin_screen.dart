9440432ccd6ceb40532ca478342b6c3f3366f68b
SuhelMakkad <MAKADSUHEL11@GMAIL.COM>
2022-05-31 22:55:14 +0530

    styling update for login screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -8,90 +8,102 @@ class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
   static const routeName = "/sign-in";
-  final headingText = "Welcome back!";
+  final headingText = "!";
   final subHeadingText = "Sign in to your account";
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final themeData = Theme.of(context);
+    final textTheme = themeData.textTheme;
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  headingText,
-                  style: textTheme.headline4?.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                text: "Welcome back",
+                style: textTheme.headline4?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  subHeadingText,
-                  style: textTheme.headline6,
-                ),
-              ),
-              const SizedBox(height: 24),
-              const InputWithLabel(
-                label: "Email",
-                textInputAction: TextInputAction.next,
-              ),
-              const SizedBox(height: 12),
-              const InputWithLabel(
-                label: "Password",
-                isTextHideable: true,
-              ),
-              const SizedBox(height: 12),
-              GestureDetector(
-                onTap: () {},
-                child: const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Forgot password?",
-                    textAlign: TextAlign.end,
-                    // style: textTheme.headline6,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(10),
-                  ),
-                  child: const Text(
-                    "Continue",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("Don't have an account?"),
-                  const SizedBox(width: 6),
-                  GestureDetector(
-                    onTap: () {},
-                    child: const Text(
-                      "Sign up",
-                      style: TextStyle(decoration: TextDecoration.underline),
+                  TextSpan(
+                    text: "!",
+                    style: textTheme.headline4?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: themeData.colorScheme.secondary,
                     ),
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              subHeadingText,
+              style: textTheme.headline6,
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 24),
+            const InputWithLabel(
+              label: "Email",
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(height: 12),
+            const InputWithLabel(
+              label: "Password",
+              isTextHideable: true,
+            ),
+            const SizedBox(height: 2),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: themeData.colorScheme.secondary,
+                  ),
+                  child: const Text(
+                    "Forgot password?",
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 6),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(10),
+                ),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(height: 6),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account?"),
+                const SizedBox(width: 6),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
