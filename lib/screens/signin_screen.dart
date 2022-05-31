039593cd7376d53915f83bd52d8ff7629554b9b3
SuhelMakkad <MAKADSUHEL11@GMAIL.COM>
2022-05-31 23:41:49 +0530

    added scroll

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -20,139 +20,146 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
     final textTheme = themeData.textTheme;
+
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            RichText(
-              text: TextSpan(
-                text: _authMode == AuthMode.login
-                    ? "Welcome back"
-                    : "Welcome aboard",
-                style: textTheme.headline4?.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                children: [
-                  TextSpan(
-                    text: "!",
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: _authMode == AuthMode.login
+                        ? "Welcome back"
+                        : "Welcome aboard",
                     style: textTheme.headline4?.copyWith(
                       fontWeight: FontWeight.bold,
-                      color: themeData.colorScheme.secondary,
+                      color: Colors.black,
                     ),
+                    children: [
+                      TextSpan(
+                        text: "!",
+                        style: textTheme.headline4?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: themeData.colorScheme.secondary,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              _authMode == AuthMode.login
-                  ? "Sign in to your account"
-                  : "Create a new account",
-              style: textTheme.titleMedium,
-              textAlign: TextAlign.start,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            const InputWithLabel(
-              label: "Email",
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            InputWithLabel(
-              label: "Password",
-              obscureText: true,
-              textInputAction: _authMode == AuthMode.login
-                  ? TextInputAction.done
-                  : TextInputAction.next,
-            ),
-            if (_authMode == AuthMode.signup)
-              Column(
-                children: const [
-                  SizedBox(
-                    height: 12,
-                  ),
-                  InputWithLabel(
-                    label: "Confirm",
-                    obscureText: true,
-                    allowObscureToggle: true,
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                ],
-              ),
-            const SizedBox(
-              height: 2,
-            ),
-            if (_authMode == AuthMode.login)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Forgot password?",
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-            const SizedBox(
-              height: 6,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(10),
                 ),
-                child: Text(
-                  _authMode == AuthMode.login ? "Continue" : "Create Account",
-                  style: const TextStyle(fontSize: 18),
+                const SizedBox(
+                  height: 8,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
                 Text(
                   _authMode == AuthMode.login
-                      ? "Don't have an account?"
-                      : "Already have an account?",
+                      ? "Sign in to your account"
+                      : "Create a new account",
+                  style: textTheme.titleMedium,
+                  textAlign: TextAlign.start,
                 ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      if (_authMode == AuthMode.login) {
-                        _authMode = AuthMode.signup;
-                      } else {
-                        _authMode = AuthMode.login;
-                      }
-                    });
-                  },
-                  child: Text(
-                    _authMode == AuthMode.login ? "Sign up" : "Login",
-                    style: const TextStyle(
-                      decoration: TextDecoration.underline,
+                const SizedBox(
+                  height: 24,
+                ),
+                const InputWithLabel(
+                  label: "Email",
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                InputWithLabel(
+                  label: "Password",
+                  obscureText: true,
+                  textInputAction: _authMode == AuthMode.login
+                      ? TextInputAction.done
+                      : TextInputAction.next,
+                ),
+                if (_authMode == AuthMode.signup)
+                  Column(
+                    children: const [
+                      SizedBox(
+                        height: 12,
+                      ),
+                      InputWithLabel(
+                        label: "Confirm",
+                        obscureText: true,
+                        allowObscureToggle: true,
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                    ],
+                  ),
+                const SizedBox(
+                  height: 2,
+                ),
+                if (_authMode == AuthMode.login)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Forgot password?",
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                const SizedBox(
+                  height: 6,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(10),
+                    ),
+                    child: Text(
+                      _authMode == AuthMode.login
+                          ? "Continue"
+                          : "Create Account",
+                      style: const TextStyle(fontSize: 18),
                     ),
                   ),
                 ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      _authMode == AuthMode.login
+                          ? "Don't have an account?"
+                          : "Already have an account?",
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          if (_authMode == AuthMode.login) {
+                            _authMode = AuthMode.signup;
+                          } else {
+                            _authMode = AuthMode.login;
+                          }
+                        });
+                      },
+                      child: Text(
+                        _authMode == AuthMode.login ? "Sign up" : "Login",
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
