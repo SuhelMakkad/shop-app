4b7dca369be84758db7ae084005e44711ed5875f
SuhelMakkad <MAKADSUHEL11@GMAIL.COM>
2022-06-12 16:00:27 +0530

    animating image on details page

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -15,48 +15,80 @@ class ProductDetailScreen extends StatelessWidget {
     final product = products.findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      // appBar: AppBar(
+      //   title: Text(product.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              centerTitle: true,
+              background: Hero(
                 tag: product.id,
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
+                child: Stack(
+                  children: [
+                    Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                      height: 400,
+                    ),
+                    Container(
+                      height: 400,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          gradient: LinearGradient(
+                              begin: FractionalOffset.topCenter,
+                              end: FractionalOffset.bottomCenter,
+                              colors: [
+                                const Color.fromARGB(255, 0, 0, 0)
+                                    .withOpacity(0.0),
+                                Colors.black.withOpacity(0.8),
+                              ],
+                              stops: const [
+                                0.75,
+                                1.0
+                              ])),
+                    )
+                  ],
                 ),
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              product.price.toString(),
-              style: const TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                product.description,
+              Text(
+                product.price.toString(),
                 textAlign: TextAlign.center,
-                softWrap: true,
-                style: const TextStyle(),
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
               ),
-            ),
-          ],
-        ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                  style: const TextStyle(),
+                ),
+              ),
+              const SizedBox(
+                height: 1000,
+              ),
+            ]),
+          )
+        ],
       ),
     );
   }
