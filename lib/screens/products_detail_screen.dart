3e19c771481800209f0fbd6b082776f83d09e60e
SuhelMakkad <MAKADSUHEL11@GMAIL.COM>
2022-06-12 17:03:54 +0530

    added buy now and add to card button

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/cart_screen.dart';
 
 import '../providers/products.dart';
+import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({Key? key}) : super(key: key);
 
   static const routeName = "/product-detail";
+
+  void _addItemToCart({
+    required BuildContext ctx,
+    required Cart cart,
+    required Product product,
+    bool showSnakBar = true,
+  }) {
+    cart.addItem(
+      productId: product.id,
+      price: product.price,
+      title: product.title,
+    );
+
+    if (!showSnakBar) return;
+
+    ScaffoldMessenger.of(ctx).hideCurrentSnackBar();
+    ScaffoldMessenger.of(ctx).showSnackBar(
+      SnackBar(
+        content: const Text("Item Added to Cart!"),
+        duration: const Duration(seconds: 2),
+        action: SnackBarAction(
+          label: "UNDO",
+          onPressed: () => cart.removeSingleItem(product.id),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFABButton({
+    required String lable,
+    required Color color,
+    required VoidCallback onPressed,
+  }) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          padding: const EdgeInsets.all(12),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          lable,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +67,46 @@ class ProductDetailScreen extends StatelessWidget {
     final products = Provider.of<Products>(context, listen: false);
     final product = products.findById(productId);
 
+    final cart = Provider.of<Cart>(context, listen: false);
+
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(product.title),
-      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildFABButton(
+              lable: "Buy Now",
+              color: const Color.fromARGB(255, 255, 216, 20),
+              onPressed: () {
+                _addItemToCart(
+                  ctx: context,
+                  cart: cart,
+                  product: product,
+                  showSnakBar: false,
+                );
+
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            _buildFABButton(
+              lable: "Add to Cart",
+              color: const Color.fromARGB(255, 255, 164, 28),
+              onPressed: () {
+                _addItemToCart(
+                  ctx: context,
+                  cart: cart,
+                  product: product,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -71,7 +71,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
 
     return ClipRRect(
